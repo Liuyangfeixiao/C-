fe6efc19ab082397e750da94800f35c7f338d567
刘阳飞晓 <2391461132@qq.com>
2020-03-09 09:38:17 +0800

    leetcode 和 codeWar 刷题

--- HomeWork/Solution.h
+++ HomeWork/Solution.h
@@ -6,11 +6,30 @@
 #define HOMEWORK_SOLUTION_H
 
 #include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <bits/unordered_set.h>
 using namespace std;
+
+struct ListNode{
+    ListNode* next;
+    int val;
+    ListNode(int x): val(x), next(nullptr){}
+};
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p);
     vector<string> arrayIn(vector<string> &array1, vector<string>& array2);
+    vector<vector<int>> merge(vector<vector<int>>& intervals);
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval);
+    void sortColors(vector<int>& nums);
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2);
+    ListNode* insertionSortList(ListNode* head);
+    ListNode* sortList(ListNode* head);
+    ListNode* merge(ListNode* list_1, ListNode* list_2);
+    ListNode* cut(ListNode* head, int n);
+
 };
 
 
