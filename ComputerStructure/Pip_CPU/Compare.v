4eb1ff04f4f6d9100ac3c99550e5d5966480f744
刘阳飞晓 <2391461132@qq.com>
2020-05-30 18:37:27 +0800

    创建Compare（未编译）

---
+++ ComputerStructure/Pip_CPU/Compare.v
@@ -0,0 +1,17 @@
+module Compare(
+    input signed [31:0] A,
+    input signed [31:0] B,
+    output reg [1:0] res;
+);
+always @(*) begin
+    if (A < B) begin
+        res = 2'b00;
+    end
+    else if(A == B)
+        res = 2'b01;
+    else
+        res = 2'b10;
+end
+
+
+endmodule // Compare
