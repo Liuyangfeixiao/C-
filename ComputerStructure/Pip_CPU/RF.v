de25dbc50853ce088e1cf684ab1f3793f92f33ac
刘阳飞晓 <2391461132@qq.com>
2020-05-31 18:44:31 +0800

    将最后的regsel取消

--- ComputerStructure/Pip_CPU/RF.v
+++ ComputerStructure/Pip_CPU/RF.v
@@ -4,8 +4,9 @@ module RF(   input         clk,
                input  [4:0]  A1, A2, A3, 
                input  [31:0] WD, 
                output [31:0] RD1, RD2,
-               input  [4:0]  reg_sel,
-               output [31:0] reg_data);
+               //input  [4:0]  reg_sel,
+               //output [31:0] reg_data
+               );
 
   reg [31:0] rf[31:0];
 
@@ -30,6 +31,6 @@ module RF(   input         clk,
 
   assign RD1 = (A1 != 0) ? rf[A1] : 0;
   assign RD2 = (A2 != 0) ? rf[A2] : 0;
-  assign reg_data = (reg_sel != 0) ? rf[reg_sel] : 0; 
+  //assign reg_data = (reg_sel != 0) ? rf[reg_sel] : 0; 
 
 endmodule 
