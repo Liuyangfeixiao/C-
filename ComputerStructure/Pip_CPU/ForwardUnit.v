17616b73f11bb0cd211bdb98ed1767aa102b4382
刘阳飞晓 <2391461132@qq.com>
2020-05-31 22:57:22 +0800

    将EXEMEM改为EXMEM

--- ComputerStructure/Pip_CPU/ForwardUnit.v
+++ ComputerStructure/Pip_CPU/ForwardUnit.v
@@ -4,7 +4,7 @@ module ForwardUnit(
     input[4:0] RtAddr_IFID,
     input[4:0] RsAddr_IDEX,
     input[4:0] RtAddr_IDEX,
-    input[4:0] RdAddr_EXEMEM,
+    input[4:0] RdAddr_EXMEM,
     input[4:0] RdAddr_MEMWB,
     input RegWrite_EXMEM,
     input RegWrite_MEMWB,
@@ -32,27 +32,27 @@ always @(*) begin
     ForwardD <= 0;
     ForwardE <= 0;
     //Data Forward
-    if (RegWrite_EXMEM && (RdAddr_EXEMEM != 0) && (RdAddr_EXEMEM == RsAddr_IDEX)) begin
+    if (RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RsAddr_IDEX)) begin
         ForwardA = 2'b01; //EXEMEM to EXE Rs
     end
-    if (RegWrite_EXMEM && (RdAddr_EXEMEM != 0) && (RdAddr_EXEMEM == RtAddr_IDEX)) begin
+    if (RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RtAddr_IDEX)) begin
         ForwardB = 2'b01; //EXEMEM to EXE Rt
     end
-    if (RegWrite_MEMWB && (RdAddr_MEMWB != 0) && !(RegWrite_EXMEM && (RdAddr_EXEMEM != 0) && (RdAddr_EXEMEM == RsAddr_IDEX)) && (RdAddr_MEMWB == RsAddr_IDEX)) begin
+    if (RegWrite_MEMWB && (RdAddr_MEMWB != 0) && !(RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RsAddr_IDEX)) && (RdAddr_MEMWB == RsAddr_IDEX)) begin
         ForwardA = 2'b10; //MEMWB to EXE Rs
     end
-    if(RegWrite_MEMWB && (RdAddr_MEMWB != 0) && !(RegWrite_EXMEM && (RdAddr_EXEMEM != 0) && (RdAddr_EXEMEM == RtAddr_IDEX)) && (RdAddr_MEMWB == RtAddr_IDEX))
+    if(RegWrite_MEMWB && (RdAddr_MEMWB != 0) && !(RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RtAddr_IDEX)) && (RdAddr_MEMWB == RtAddr_IDEX))
         ForwardB = 2'b10; //MEMWB to EXE Rt
     
 
     //Bracnch Forward
-    if (RegWrite_EXMEM && (RdAddr_EXEMEM != 0) && (RdAddr_EXEMEM == RsAddr_IFID) && (Opcode = 6'h04 || Opcode == 6'h05)) begin
+    if (RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RsAddr_IFID) && (Opcode = 6'h04 || Opcode == 6'h05)) begin
         ForwardC = 1;
     end
-    if(RegWrite_EXMEM && (RdAddr_EXEMEM != 0) && (RdAddr_EXEMEM == RtAddr_IFID) && (Opcode = 6'h04 || Opcode == 6'h05)) 
+    if(RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RtAddr_IFID) && (Opcode = 6'h04 || Opcode == 6'h05)) 
         ForwardD = 1;
     
-    if (RegWrite_EXMEM && (RdAddr_EXEMEM != 0) && (RdAddr_EXEMEM == RsAddr_IFID) && (Opcode = 6'h01 || Opcode == 6'h06 || Opcode == 6'h07)) begin
+    if (RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RsAddr_IFID) && (Opcode = 6'h01 || Opcode == 6'h06 || Opcode == 6'h07)) begin
         ForwardE = 1;
     end
 
