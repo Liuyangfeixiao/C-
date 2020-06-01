f5f2a58d2271e206dd85c68a0ef47272bc21128f
刘阳飞晓 <2391461132@qq.com>
2020-06-01 14:06:46 +0800

    添加Forward消息提示

--- ComputerStructure/Pip_CPU/ForwardUnit.v
+++ ComputerStructure/Pip_CPU/ForwardUnit.v
@@ -33,27 +33,31 @@ always @(*) begin
     ForwardE <= 0;
     //Data Forward
     if (RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RsAddr_IDEX)) begin
-        ForwardA = 2'b01; //EXEMEM to EXE Rs
+        ForwardA <= 2'b01; //EXEMEM to EXE Rs
+	$display("A EX Forward to Rs");
     end
     if (RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RtAddr_IDEX)) begin
-        ForwardB = 2'b01; //EXEMEM to EXE Rt
+        ForwardB <= 2'b01; //EXEMEM to EXE Rt
+	$display("A EX Forward to Rt");
     end
     if (RegWrite_MEMWB && (RdAddr_MEMWB != 0) && !(RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RsAddr_IDEX)) && (RdAddr_MEMWB == RsAddr_IDEX)) begin
-        ForwardA = 2'b10; //MEMWB to EXE Rs
+        ForwardA <= 2'b10; //MEMWB to EXE Rs
+	$display("A MEM Forward to Rs");
     end
-    if(RegWrite_MEMWB && (RdAddr_MEMWB != 0) && !(RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RtAddr_IDEX)) && (RdAddr_MEMWB == RtAddr_IDEX))
-        ForwardB = 2'b10; //MEMWB to EXE Rt
-    
+    if(RegWrite_MEMWB && (RdAddr_MEMWB != 0) && !(RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RtAddr_IDEX)) && (RdAddr_MEMWB == RtAddr_IDEX)) begin
+        ForwardB <= 2'b10; //MEMWB to EXE Rt
+	$display("A MEM Forward to Rt");
+    end
 
     //Bracnch Forward
     if (RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RsAddr_IFID) && (Opcode == 6'h04 || Opcode == 6'h05)) begin
-        ForwardC = 1;
+        ForwardC <= 1;
     end
     if(RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RtAddr_IFID) && (Opcode == 6'h04 || Opcode == 6'h05)) 
-        ForwardD = 1;
+        ForwardD <= 1;
     
     if (RegWrite_EXMEM && (RdAddr_EXMEM != 0) && (RdAddr_EXMEM == RsAddr_IFID) && (Opcode == 6'h01 || Opcode == 6'h06 || Opcode == 6'h07)) begin
-        ForwardE = 1;
+        ForwardE <= 1;
     end
 
 
