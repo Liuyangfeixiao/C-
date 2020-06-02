252ac6502187ef5c9b2aa5dbde4e4b9d559ee136
刘阳飞晓 <2391461132@qq.com>
2020-06-02 15:55:52 +0800

    添加branch的hazard

--- ComputerStructure/Pip_CPU/HazardUnit.v
+++ ComputerStructure/Pip_CPU/HazardUnit.v
@@ -47,6 +47,20 @@ always @(*) begin
         PCWre = 0;
         $display("A hazard for blez, bltz, bgez, bgtz occur");
     end
+    else if(RegWrite_IDEX && (RegWriteAddr_IDEX == RsAddr_IFID || RegWriteAddr_IDEX == RtAddr_IFID) && (Opcode_IFID == 6'h04 || Opcode_IFID == 6'h05 )) begin
+        IFID_Stall = 1;
+        //IFID_Flush = 0;
+        IDEX_Flush = 1;
+        PCWre = 0;
+        $display("A hazard for beq or bnq occur");
+    end
+    else if(RegWrite_IDEX && (RegWriteAddr_IDEX == RsAddr_IFID) && (Opcode_IFID == 6'h01 || Opcode_IFID == 6'h06 || Opcode_IFID == 6'h07)) begin
+        IFID_Stall = 1;
+        //IFID_Flush = 0;
+        IDEX_Flush = 1;
+        PCWre = 0;
+        $display("A hazard for blez, bltz, bgez, bgtz occur");
+    end
     else if(RegWrite_IDEX && (RegWriteAddr_IDEX == RsAddr_IFID) && (Opcode_IFID == 6'h00 && (Func_IFID == 6'd8 || Func_IFID == 6'd9))) begin
  	IFID_Stall = 1;
         //IFID_Flush = 0;
