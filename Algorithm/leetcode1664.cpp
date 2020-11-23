16f70040a5a6c03fe93c5ab2b87f2d5fd7c7f1fd
liuyangfeixiao <2391461132@qq.com>
2020-11-23 20:20:26 +0800

    生成平衡数组的方案

---
+++ Algorithm/leetcode1664.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int waysToMakeFair(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> s1(n+1), s2(n+1);
+    for (int i = 1; i <= n; i++) {
+        s1[i] = s1[i-1], s2[i] = s2[i-1];
+        if (i % 2) s1[i] += nums[i-1];
+        else s2[i] += nums[i-1];
+    }
+    int res = 0;
+    for (int i = 1; i <= n; i++) {
+        int odd = s1[i-1] + s2[n] - s2[i];
+        int even = s2[i-1] + s1[n] - s1[i];
+        if (odd == even) {
+            res++;
+        }
+    }
+
+    return res;
+
+}
