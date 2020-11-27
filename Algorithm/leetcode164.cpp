f4c80327167fb9049b9d63c3b1687589ac9438eb
liuyangfeixiao <2391461132@qq.com>
2020-11-27 21:09:50 +0800

    最大间距

--- Algorithm/leetcode164.cpp
+++ Algorithm/leetcode164.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 int maximumGap(vector<int>& nums) {
+    int n = nums.size();
+    if (n < 2)
+        return 0;
     sort(nums.begin(), nums.end());
     int ans = nums[1] - nums[0];
     for (int i = 2; i < n; i++) {
