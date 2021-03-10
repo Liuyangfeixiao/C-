6437539af5ac2199372ac4dc0050b85c4f0fa6fc
liuyangfeixiao <2391461132@qq.com>
2021-03-10 14:43:26 +0800

    原地旋转矩阵

--- Algorithm/leetcode48.cpp
+++ Algorithm/leetcode48.cpp
@@ -17,7 +17,7 @@ void rotate(vector<vector<int>>& matrix) {
 }
 
 /*先转置，再翻转*/
-
+/*顺时针90度为先沿着主对角线转置，再沿着垂直轴翻转*/
 void rotate2(vector<vector<int>>& matrix) {
     int n = matrix.size();
     for (int i = 0; i < n; i++) {
@@ -28,6 +28,37 @@ void rotate2(vector<vector<int>>& matrix) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n / 2; ++j) {
             swap(matrix[i][j], matrix[i][n-1-j]);
+        }
+    }
+}
+
+/*逆时针90度为先沿着主对角线转置，再沿着水平线翻转*/
+void rotate3(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n; ++i) {
+        for (int j = i+1; j < n; ++j) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    for (int i = 0; i < n/2; ++i) {
+        for (int j = 0; j < n; ++j) {
+            swap(matrix[i][j], matrix[n-1-i][j]);
+        }
+    }
+}
+
+/*逆时针旋转180度*/
+/*先水平翻转，后垂直翻转*/
+void rotate4(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n / 2; ++j) {
+            swap(matrix[i][j], matrix[i][n-1-j]);
+        }
+    }
+    for (int i = 0; i < n/2; ++i) {
+        for (int j = 0; j < n; ++j) {
+            swap(matrix[i][j], matrix[n-1-i][j]);
         }
     }
 }
