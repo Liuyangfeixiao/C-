c12c9238f78195788ef900e917fe473da335b054
liuyangfeixiao <2391461132@qq.com>
2021-03-25 12:12:17 +0800

    删除排序列表中的重复元素

---
+++ Algorithm/leetcode82.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* cur = dummy;
+        while (cur->next && cur->next->next) {
+            if (cur->next->val == cur->next->next->val) {
+                int x = cur->next->val;
+                while (cur->next && cur->next->val == x) {
+                    cur->next = cur->next->next;
+                }
+            }
+            else cur = cur->next;
+        }
+
+        return dummy->next;
+    }
+};
