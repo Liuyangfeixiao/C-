c25f3cd36008ec1e56a62dcb2e34e519c93f9889
Liuyang <2391461132@qq.com>
2021-08-09 11:21:26 +0800

    动归解决超级丑数问题

--- Algorithm/leetcode313.cpp
+++ Algorithm/leetcode313.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 using namespace std;
 
 class Solution {
@@ -21,5 +22,27 @@ public:
             
         }
         return answer;
+    }
+    // DP + n pointer
+    int nthSuperUglyNum(int n, vector<int>& primes) {
+        vector<int> dp(n + 1);
+        dp[1] = 1;
+        int m = primes.size();
+        vector<int> pointer(m, 1);
+        for (int i = 2; i <= n; ++i) {
+            vector<int> nums(m);
+            int minNum = INT_MAX;
+            for (int j = 0; j < m; ++j) {
+                nums[j] = dp[pointer[j]] * primes[j];
+                minNum = min(minNum, nums[j]);
+            }
+            dp[i] = minNum;
+            for (int j = 0; j < m; ++j) {
+                if (minNum == nums[j])
+                    pointer[j]++;
+            }
+        }
+
+        return dp[n];
     }
 };
